5146f0659065e0dac7594d61cda5a8f4ce0460ec
Yasin Korkmaz <yasinkorkmaz.dev@gmail.com>
2024-06-06 04:27:07 +0300

    Add User Validation Rules

--- app/Http/Requests/UpdateUserRequest.php
+++ app/Http/Requests/UpdateUserRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\ValidationException;
 
 class UpdateUserRequest extends FormRequest
 {
@@ -22,7 +23,30 @@ class UpdateUserRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => 'nullable|string|max:255',
+            'email' => 'nullable|email|unique:users,email',
         ];
+    }
+
+    /**
+     * Handle a passed validation attempt.
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    protected function passedValidation()
+    {
+        $hasValidInput = false;
+        foreach ($this->rules() as $field => $rule) {
+            if (!is_null($this->input($field))) {
+                $hasValidInput = true;
+                break;
+            }
+        }
+
+        if (!$hasValidInput) {
+            throw ValidationException::withMessages([
+                'general' => 'At least one field must be provided.'
+            ]);
+        }
     }
 }
